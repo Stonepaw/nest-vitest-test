4c9d4bef085b3d36a1ce96d1dcab389d93be97e3
Andrew Feltham <andrew.feltham3@gmail.com>
2022-04-11 19:57:26 -0600

    example: setup resolve aliases

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite'
+import * as path from "path";
+
+const config = require('./tsconfig.base.json')
+
+const paths = config.compilerOptions.paths;
+
+const aliases = {}
+
+for(const key in paths) {
+  aliases[key] = path.join(__dirname, paths[key][0])
+}
+
+export default defineConfig({
+  resolve: {
+    alias: aliases,
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+  },
+})
